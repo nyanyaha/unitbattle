297e702609afe19fd3990793ea3c87cf52474e86
nyanyaha <nanaha999@gmail.com>
2019-09-19 18:53:47 +0900

    テスト用のファイルをしばらく更新していなかったので更新。 ユニットを2つ生成してバトルを行うテスト。

--- test/testUnitFactory.sol
+++ test/testUnitFactory.sol
@@ -9,21 +9,51 @@ contract TestUnitFactory
 {
 	UnitFactory unitFactory = UnitFactory(DeployedAddresses.UnitFactory());
 
-	function testcriateUnit() public {
-		unitFactory.createUnit("test");
+	function errorPrint(int _code, string memory _str) internal {
+			Assert.equal(0, _code, _str);
+	}
 
-		uint[] memory unitid = unitFactory.getUnitIdByOwner(msg.sender);
-
-		uint result = 99999;
-		if(unitid.length > 0)
+	function resetUnit() internal returns(int) {
+		uint[] memory unitids = unitFactory.getUnitIdByOwner(address(this));
+		if( unitids.length <= 0 )
 		{
-			result = unitid[0];
+			return 0;
 		}
 
-		//result = uint(unitFactory.getUnitAddr(0));
-		result = unitFactory.getUnitNum();
+		unitFactory.resetUnit(unitids[0]);
+		return 1;
+	}
 
-		Assert.equal(100, result, "unit create test.");
+	function testcriateUnit() public {
+		// 所持ユニットが残っているかもしれないので初期化
+		resetUnit();
+
+		// 作成->削除->作成で複数ユニット生成
+		unitFactory.createUnit("test");
+		int r = resetUnit();
+		if( r <= 0 )
+		{
+			errorPrint(-1, "unit create error.");
+			return;
+		}
+
+		// 2体目は削除しないのでrestUnitを呼ばずに直接unitidを取得
+		unitFactory.createUnit("test2");
+		uint[] memory unitids = unitFactory.getUnitIdByOwner(address(this));
+		if( unitids.length <= 0 )
+		{
+			errorPrint(-2, "unit create error.");
+			return;
+		}
+
+		uint unitid = unitids[0];
+
+		// 戦闘
+		unitFactory.battleStartRandom(unitid);
+
+		// 終わったらイベントを確認したいのでAssertで止める
+		uint result = 1;
+		Assert.equal(0, result, "finish");
 	}
 
 }
